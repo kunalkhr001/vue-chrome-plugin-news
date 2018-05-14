e0f7d4f7fcf4a6ba826b18fbba3fc44204aed151
kunalkhr001 <kunalkhr001@gmail.com>
2018-05-14 20:26:11 +0530

    plugin initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <item></item>
+  </div>
+</template>
+
+<script>
+import Item from './components/item.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'item': Item
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+@import "./assets/css/mini-default.css";
+</style>
